e9cd556990cb03e55f39400a54df19c859a2dfb7
Tiemani <116328962+Tiemani@users.noreply.github.com>
2024-12-12 10:21:07 +0100

    Add files via upload

---
+++ finly/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default __CONFIG__
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: [
+// ...
+],
+theme: {
+// ...
+},plugins: [require('daisyui')],
+};
