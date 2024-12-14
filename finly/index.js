719fc50cf7c15524a03e81eb315ec17a88bc9f3b
Tiemani <116328962+Tiemani@users.noreply.github.com>
2024-12-14 16:45:10 +0100

    Add files via upload

--- finly/index.js
+++ finly/index.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
+
 require('dotenv').config();
 require('./libs/dbConnect');
+
+const userRouter = require('./routes/user.route');
+
 const app = express();
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
 res.render('index', { message: 'Hello From Node.js' });
 });
+
+app.use('/users', userRouter);
+
 app.get('/contact', (req, res) => {
 res.render('index', { message: 'The Contact Page' });
 });
+
 app.get('/about', (req, res) => {
 res.render('index', { message: 'The About Page' });
-});app.get('*', (req, res) => {
+});
+
+app.get('*', (req, res) => {
 res.status(404).render('index', { message: 'Not Found' });
 });
 
