1bb88823b6f054c4abdd662a7e20318f6d6d8c96
Tiemani <116328962+Tiemani@users.noreply.github.com>
2025-01-04 13:44:52 +0100

    Add files via upload

--- finly/controllers/user.controller.js
+++ finly/controllers/user.controller.js
@@ -1,20 +1,53 @@
-const User = require('../libs/models/user.model');const createUser = async (req, res) => {
-    await User.create({
-    email: 'nathan@mail.com',
-    password: 'password',
+const User = require('../libs/models/user.model');
+const bcrypt = require('bcrypt');
+
+const { body, validationResult } = require('express-validator');
+
+const validateSignup = [
+body('email', 'Email must not be empty').notEmpty(),
+body('password', 'Password must not be empty').notEmpty(),
+body('password', 'Password must be 6+ characters long').isLength({ min: 6 }),
+body('repeatPassword', 'Repeat Password must not be empty').notEmpty(),
+body('repeatPassword', 'Passwords do not match').custom((value, { req }) => (value === req.body.password)),
+];
+
+const signup = async (req, res) => {
+const validationErrors = validationResult(req);
+if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array();
+    req.flash('errors', errors);
+    req.flash('data', req.body);
+    return res.redirect('/signup');
+}
+
+const { email, password } = req.body;
+const query = { email };
+
+const existingUser = await User.findOne(query);
+if (existingUser) {
+    req.flash('data', req.body);
+    req.flash('info', {
+    message: 'Email is already registered. Try to login instead',
+    type: 'error'
+    })
+    res.redirect('/signup');
+} else {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+    email,
+    password: hashedPassword,
+    };
+    const result = await User.create(user);
+    req.session.userId = result._id;
+    req.flash('info', {
+    message: 'Signup Successful',
+    type: 'success'
     });
-    res.render('user', { message: 'User Created', user: null });
-    };
-    const getUser = async (req, res) => {
-    const user = await User.findOne({ email: 'nathan@mail.com' });
-    res.render('user', { message: 'User Retrieved', user: user });
-    };
-    const deleteUser = async (req, res) => {
-    await User.findOneAndDelete({ email: 'nathan@mail.com' });
-    res.render('user', { message: 'User Deleted', user: null });
-    };
-    module.exports = {
-    getUser,
-    createUser,
-    deleteUser,
-    };
+    res.redirect('/dashboard');
+}
+};
+
+module.exports = {
+signup,
+validateSignup
+};
--- finly/index.js
+++ finly/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
-const dashboardRouter = require('./routes/dashboard.route');
+const flash = require('connect-flash');
 
 require('dotenv').config();
 require('./libs/dbConnect');
 
 const userRouter = require('./routes/user.route');
+const dashboardRouter = require('./routes/dashboard.route');
 
 const app = express();
 
@@ -15,22 +16,19 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.static('./public'));
-
-// .Implementing a session-based authentication..
-app.use(morgan('dev'));
-app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
+
 app.use(
-    session({
-        secret: process.env.AUTH_SECRET,
-        saveUninitialized: true,
-        resave: false,
-    })
+session({
+    secret: process.env.AUTH_SECRET,
+    saveUninitialized: true,
+    resave: false,
+})
 );
 
+app.use(flash());
 
-app.use('/users', userRouter);
-//To add dashboard path to this code
+app.use('/', userRouter);
 app.use('/dashboard', dashboardRouter);
 
 app.get('*', (req, res) => {
